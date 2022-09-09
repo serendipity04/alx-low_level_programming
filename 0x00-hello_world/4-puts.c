93426cad575484c1236d6f5e4a08141319cba989
Obinka <divineobk@gmail.com>
2022-09-08 22:29:47 -0700

    c files

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+
+ * Description: main - Prints out a string to.
+
+ * Return: 0 if successful.
+
+ */
+
+int main(void)
+
+{
+
+	puts("\"Programming is like building a multilingual puzzle");
+
+
+
+	return (0);
+
+}
