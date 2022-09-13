69d50c0c1ba56b017eb7c544cebe4236a0f4d998
Obinka <divineobk@gmail.com>
2022-09-12 20:27:30 -0700

    comb4

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: print int combinations
+ *
+ * Return: returns 0
+ */
+
+int main(void)
+{
+	int a, b, c;
+	
+	for (a = '0'; a <= '9'; a++)
+	{
+		for (b = '0'; b <= '9'; b++)
+		{
+			for (c = '0'; c <= '9'; c++)
+			{
+				if( a < b && b < c )
+				{
+					putchar(a);
+					putchar(b);
+					putchar(c);
+
+					if (a != '7')
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+
+	putchar('\n');
+	return (0);
+}
--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -14,6 +14,7 @@ int main(void)
 
 	for (; n <= 57; n++)
 		putchar(n);
+	putchar('\n');
 
 	return (0);
 
