63551b424d8e6a645f9e611e62904982ec2337bf
Obinka <divineobk@gmail.com>
2022-09-12 20:50:21 -0700

    finished all tasks

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -11,14 +11,14 @@
 int main(void)
 {
 	int a, b, c;
-	
+
 	for (a = '0'; a <= '9'; a++)
 	{
 		for (b = '0'; b <= '9'; b++)
 		{
 			for (c = '0'; c <= '9'; c++)
 			{
-				if( a < b && b < c )
+				if (a < b && b < c)
 				{
 					putchar(a);
 					putchar(b);
