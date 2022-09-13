7ae67e26ef13bf7a2bf0bdf293a31d47460991df
Obinka <divineobk@gmail.com>
2022-09-12 18:50:31 -0700

    numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: prints numbers using putchar
+ *
+ * Return: returns 0 when a success
+ */
+
+int main(void)
+{
+	int n = 0;
+
+	for (; n < 10; n++)
+		putchar(n);
+
+	return (0);
+
+}
