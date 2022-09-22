e0a6c5b00332f374e7b4d63c7bb5d04efb7e1c10
Ugo <78849868+serendipity04@users.noreply.github.com>
2022-09-22 13:45:36 +0100

    print alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_alphabet - prints the alphabet
+ * 
+ * Description: prints the alphabet
+ * 
+ * Return: always 0
+ */
+
+int main(void)
+{
+    char c = 'a';
+    char n = '\n';
+    while(; c <= 'z'; c ++)
+    {
+        _putchar(c);
+    }
+
+    _putchar(n);
+
+    return (0);
+}
