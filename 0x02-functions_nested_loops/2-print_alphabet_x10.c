b351cbc1d339dd7ed334e6ac35700968416bdcc2
Ugo <78849868+serendipity04@users.noreply.github.com>
2022-09-23 01:06:17 +0100

    exe

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -16,8 +16,9 @@ void print_alphabet_x10(void)
 
     while (count++ <=9)
     {
-        for (c = 'a'; c <= 'z'; c++)
+        for (c = 'a'; c <= 'z'; c++){
             _putchar(c);
             _putchar('\n');
+        }
     }
 }
