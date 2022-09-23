9c4591c27df4fdf2d29001dfce94a22ab5093240
Ugo <78849868+serendipity04@users.noreply.github.com>
2022-09-23 09:23:29 +0100

    task 5

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -10,13 +10,13 @@
 
 void print_alphabet_x10(void)
 {
-    int count = 0;
-    char c;
+	int count = 0;
+	char c;
 
-    while (count++ <= 9)
-    {
-        for (c = 'a'; c <= 'z'; c++)
-            _putchar(c);
-        _putchar('\n');
-    }
+	while (count++ <= 9)
+	{
+	for (c = 'a'; c <= 'z'; c++)
+		_putchar(c);
+	_putchar('\n');
+	}
 }
--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -10,7 +10,7 @@
 
 int print_sign(int n)
 {
-    if (n > 0)
+	if (n > 0)
 	{
 		_putchar('+');
 		return (1);
@@ -26,3 +26,4 @@ int print_sign(int n)
 		return (-1);
 	}
 }
+
