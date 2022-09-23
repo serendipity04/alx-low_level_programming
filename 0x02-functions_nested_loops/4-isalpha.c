6068094555d6131ddcf6741c3ed917cc2c11388e
Ugo <78849868+serendipity04@users.noreply.github.com>
2022-09-23 01:41:53 +0100

    alpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _isalpha - entry point
+ *
+ * Description: print alpha value
+ *@c: contains value to be compared
+ * Return: 0
+ */
+
+int _isalpha(int c)
+{
+    if (c > 'A' && c < 'Z')
+    {
+        return (1);
+    }
+    else{
+        return (0);
+    }
+}
