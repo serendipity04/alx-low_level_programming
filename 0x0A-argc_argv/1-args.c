73e8bab52e70e73d6a2c6e15f48cc94e15436dd6
Obinka <divineobk@gmail.com>
2022-10-03 01:46:14 -0700

    args

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - prints number 9of argument passed
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Return: int
+ */
+
+int main(int argc, char *argv[])
+{
+	if (argc > 0)
+	{
+		argv = argv;
+		printf("%d\n", argc - 1);
+	}
+	return (0);
+}
