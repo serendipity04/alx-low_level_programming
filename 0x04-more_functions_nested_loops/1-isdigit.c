84f948671b9b99c29546076e83db9a1883408e5a
Obinka <divineobk@gmail.com>
2022-10-08 00:00:26 -0700

    isdigit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _isdigit - function check for integers
+ * @c: input value to check
+ * Return: int
+ */
+
+int _isdigit(int c)
+{
+	if (c >= 48 && c <= 57)
+	{
+		return (1);
+	}
+	else
+		return (0);
+}
