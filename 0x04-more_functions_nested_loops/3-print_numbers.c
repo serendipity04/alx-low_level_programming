92e56abf7df46f05a399cb2b03a18394934cac7f
Obinka <divineobk@gmail.com>
2022-10-09 00:46:13 -0700

    commit

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_numbers - function prints numbers from 0-9
+ *
+ * Return: void
+ */
+
+void print_numbers(void)
+{
+	int n = o;
+
+	while (n < 10)
+	{
+		_putchar(n + '0')
+		n += 1;
+	}
+	_putchar('\n');
+}
